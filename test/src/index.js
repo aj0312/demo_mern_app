0efd426f7b2c1a2b5805c6441544cf95bc2ac4c2
anish <chikodianish@gmail.com>
2019-02-25 12:23:49 +0530

    learning react with linkedin learning

--- test/src/index.js
+++ test/src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const bookList = [
+    {title: 'Book1', pages: 290, author: 'Anon'},
+    {title: 'Book2', pages: 130, author: 'Abc'},
+    {title: 'Book3', pages: 420, author: 'Def'},
+]
+const Book = (book) => {
+    return (
+        <span>
+            <h4>Book title: {book.title}</h4>
+            <p>Book Pages: {book.pages}</p>
+            <p>Book author: {book.author}</p>
+        </span>
+    )
+}
+const Library = ({books}) => {
+    return (
+        <div>
+            {books.map(
+                (book, i) =>
+                    <Book
+                        key={i}
+                        title={book.title}
+                        author={book.author}
+                        pages={book.pages}
+                    />
+            )}
+        </div>
+    )
+}
+render(
+    <Library books={bookList} />,
+  document.getElementById('root')
+)
