666ea3d19ef20fd43a43a8f30b6d49e656b4cfca
anish <chikodianish@gmail.com>
2019-03-07 18:36:30 +0530

    added add and update APIs

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const router = express.Router();
 const port = process.env.PORT || 5000;
+const mongoToDoConn = require('./mongo_todo')
+let Todo = require('./todo.model')
 
 app.use(bodyParser.json())
 app.use(cors({credentials: true, origin: true}))
@@ -31,7 +33,54 @@ router.route('/users')
       })
 
 app.use('/', router)
+router.route('/', (req, res) => 
+  Todo.find(
+    (err, todos) => (err) ? console.log(err) : res.json(todos)
+  )
+)
+router.route('/:id').get(
+  (req, res) => {
+    let id = req.params.id
+    Todo.findById(id, 
+      (err, todo)  => res.json(todo)
+    )
+  }
+)
+router.route('/update/:id').post(
+  (req, res) => {
+    Todo.findById(req.params.id, 
+      (err, res) => {
+        if (!todo)
+          res.status(404).send(`data not found`)
+        else
+          todo.todo_description = req.body.todo_description
+          todo.todo_responsible = req.body.todo_responsible
+          todo.todo_priority = req.body.todo_priority
+          todo.todo_completed = req.body.todo_completed
 
+          todo.save().then(todo => {
+            res.json(`Todo updated`)
+          })
+          .catch(err => 
+            res.status(400).send(`Update not possible`)
+          )
+    })
+})
+
+router.route('/add', 
+  (req, res) => {
+    let todo = new Todo(req.body)
+    todo.save()
+        .then(
+          todo => res.status(200).json({'todo': `Successfully added new todo`})
+        )
+        .catch(
+          error => res.send(400).send(`adding new todo failed!`)
+        )
+  })
+
+  app.use('/todos', router)
+  
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
